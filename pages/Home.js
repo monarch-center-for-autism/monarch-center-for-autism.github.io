8e0f68ab87f323504b40098da1878a3e008a95d8
Jordan Pitlor <jordan@pitlor.dev>
2021-01-12 19:09:38 -0500

    change title

--- pages/Home.js
+++ pages/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <Flex direction="column" alignItems="center" w="full" pt={16}>
       <Image src={logo} mb={8} />
-      <Heading mb={4}>Welcome to Monarch Satellite</Heading>
+      <Heading mb={4}>Welcome to the Monarch Resources website</Heading>
       <Text>
         Find curated content by visiting one of the pages in the side bar
       </Text>
